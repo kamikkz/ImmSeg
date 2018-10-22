59d19d4d13a87b36eb00197e0173f4075bb3c21e
kami <jorge.suarez@interpuerto.com>
2018-10-22 16:17:46 -0500

    Laravel excel
    install

--- config/app.php
+++ config/app.php
@@ -154,6 +154,7 @@ return [
         Caffeinated\Shinobi\ShinobiServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -215,6 +216,7 @@ return [
 
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
